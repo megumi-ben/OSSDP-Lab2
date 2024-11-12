450eed8bf2f595a7a35dd24fd9d811970cfdf1ea
megumi-ben <163231066+megumi-ben@users.noreply.github.com>
2024-11-12 10:01:42 +0800

    Update SolutionTest.java

--- SolutionTest.java
+++ SolutionTest.java
@@ -1,13 +1,24 @@
+import org.junit.Test;
+import java.util.Arrays;
+import static org.junit.Assert.assertArrayEquals;
+
 public class SolutionTest {
-    public static void main(String[] args) {
+    @Test
+    public void testProductExceptSelf1() {
         Solution solution = new Solution();
-
-        int[] nums1 = {1, 2, 3, 4};
-        int[] result1 = solution.productExceptSelf(nums1);
-        System.out.println(Arrays.toString(result1)); // 预期输出: [24, 12, 8, 6]
-
-        int[] nums2 = {-1, 1, 0, -3, 3};
-        int[] result2 = solution.productExceptSelf(nums2);
-        System.out.println(Arrays.toString(result2)); // 预期输出: [0, 0, 9, 0, 0]
+        int[] nums = {1, 2, 3, 4};
+        int[] expected = {24, 12, 8, 6};
+        assertArrayEquals(expected, solution.productExceptSelf(nums));
+        System.out.println(Arrays.toString(solution.productExceptSelf(nums)));
+    }
+    @Test
+    public void testProductExceptSelf2() {
+        Solution solution = new Solution();
+        int[] nums = {-1, 1, 0, -3, 3};
+        int[] expected = {0, 0, 9, 0, 0};
+        assertArrayEquals(expected, solution.productExceptSelf(nums));
+        System.out.println(Arrays.toString(solution.productExceptSelf(nums)));
     }
 }
+
+
